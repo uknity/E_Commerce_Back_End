a9f8b82b58e560c2cab86d7b539ddfeaf0951d9f
Liz Emerson <uknity@gmail.com>
2021-04-22 20:41:47 -0400

    added product-routes; capitalized Datatypes in ProductTag model; added routes in index.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,20 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
       },
     },
     tag_id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id',
